f14b6922622e4b2831b2cb41789cd56cef0cdfa2
Sero <122702052+21Sero@users.noreply.github.com>
2024-08-05 12:43:36 +0400

    fix: about us slider

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -11,7 +11,7 @@ import teamMember3 from '../images/teamMember3.png';
 import teamMember4 from '../images/teamMember4.png';
 import teamMember5 from '../images/teamMember5.png';
 import teamMember6 from '../images/teamMember6.jpg';
-// import teamMember7 from '../images/teamMember7.jpg';
+import teamMember7 from '../images/teamMember7.jpg';
 import footerSlider1 from '../images/footerSlider1.jpg';
 import footerSlider2 from '../images/footerSlider2.jpg';
 import footerSlider3 from '../images/footerSlider3.jpg';
@@ -23,12 +23,13 @@ import { Navigation, Pagination } from "swiper/modules";
 
 function AboutUs() {
     const teamMembers = [
-        { name: "Tatev Sirunyan", role: "Founder CEO", image: teamMember1, text: "Our CEO is like a gem in the realm of jewelry CAD, constantly polishing her expertise, showering the team with care,  and occasionally just forgetting that sleep is as precious as a flawless diamond!" },
+        { name: "Tatev Sirunyan", role: "Founder CEO", image: teamMember1, text: "Our CEO is like a gem in the realm of jewelry CAD, constantly polishing her expertise, showering the team with care, and occasionally just forgetting that sleep is as precious as a flawless diamond!" },
         { name: "Elen Davtyan", role: "Co-Founder Senior CAD Professional", image: teamMember2, text: "A self-confident and enthusiastic Senior CAD professional who once dazzled on the football field, theatres and even fencing stripes, and now charms everyone with her sociable and friendly nature, making her a beloved figure in both her personal and professional circles." },
         { name: "Lilith", role: "Sketcher", image: teamMember3, text: "The ethereal artist who's as fragile as a butterfly but can knock out a masterpiece with one hand and an opponent with the other like a heavyweight champion, critiques with the precision of a surgeon, rebels against all things dull, and enjoys life like it's one big, beautiful canvas!" },
         { name: "Shushan", role: "Rendering Professional", image: teamMember4, text: "Оur rendering professional is like a kaleidoscope of energy: detail-oriented enough to spot a misplaced pixel, chatty enough to entertain a whole room, and so infectiously upbeat, they could turn a gloomy Monday into a disco party—all while making your jewelry designs look absolutely dazzling!" },
         { name: "Rima", role: "CAD Professional", image: teamMember5, text: "A brilliant and humble CAD professional whose calm and peaceful demeanor is only rivaled by her endearing reluctance to ever reveal her favorite song." },
         { name: "Suren", role: "The future CAD star", image: teamMember6, text: "The future CAD star with a talkative streak, a sugar-free sweet tooth (except for those daily six spoons in his tea), and a love for his job so strong, he's contemplating moving permanently to the office with his pillow." },
+        { name: "Mayis", role: "The future CAD star", image: teamMember7, text: "Don't let his \"Nothing\" answers fool you – our junior CAD designer is a secret weapon when it comes to 3D modeling of stunning jewelry designs. And, just for the record, he's also a champion of the \"do-it-all\" philosophy, having conquered football, fencing, and boxing (but shhhh, don't tell him we mentioned those!)." },
     ];
 
     const footerSlides = [
@@ -75,18 +76,59 @@ function AboutUs() {
                         prevEl: '.swiper-button-prev-unique',
                         nextEl: '.swiper-button-next-unique',
                     }}
-                    slidesPerView={4.25}
+                    slidesPerView={2.5}
                     spaceBetween={10}
                     loop={true}
                     className={styles.ourTeamSlider}
                     breakpoints={{
                         320: {
+                            slidesPerView: 1.25,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        410: {
                             slidesPerView: 1.5,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        510: {
+                            slidesPerView: 1.75,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        590: {
+                            slidesPerView: 2.25,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 2.5,
                             direction: 'horizontal',
                             spaceBetween: 20,
                         },
-                        768: {
-                            slidesPerView: 4,
+
+                        1024: {
+                            slidesPerView: 3.25,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        1088: {
+                            slidesPerView: 3.5,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        1100: {
+                            slidesPerView: 3,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        1440: {
+                            slidesPerView: 4.5,
+                            direction: 'horizontal',
+                            spaceBetween: 10,
+                        },
+                        1700: {
+                            slidesPerView: 5.5,
                             direction: 'horizontal',
                             spaceBetween: 10,
                         },
